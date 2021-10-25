23c40c2fc7c2dccc328f41263b63c3bfa2caeb2e
nabin <nkbhaie2017@gmail.com>
2021-10-25 12:57:11 -0600

    add first join query

--- queries.sql
+++ queries.sql
@@ -1,3 +1,4 @@
 -- write your queries here
 --joins_exercise=# SELECT * FROM owners FULL JOIN vehicles ON owners.id = vehicles.id ORDER BY vehicles.id;
-SELECT * FROM owners FULL JOIN vehicles ON owners.id = vehicles.id ORDER BY vehicles.id;
+SELECT * FROM owners FULL JOIN vehicles ON owners.id = vehicles.id ORDER BY vehicles.id; -- then do this
+SELECT * FROM owners FULL JOIN vehicles ON owners.id = owner_id; -- because above gives owner_id column
