ac9c7651bbf72813fab55f2e2ebafd625ad65137
nabin <nkbhaie2017@gmail.com>
2021-10-25 12:19:18 -0600

    add first join query

--- queries.sql
+++ queries.sql
@@ -1,1 +1,3 @@
 -- write your queries here
+--joins_exercise=# SELECT * FROM owners FULL JOIN vehicles ON owners.id = vehicles.id ORDER BY vehicles.id;
+SELECT * FROM owners FULL JOIN vehicles ON owners.id = vehicles.id ORDER BY vehicles.id;
